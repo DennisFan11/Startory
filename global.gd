40af52e1891dfe4333974991e88450d6f5b5d200
Dennis515jd <dennis515jd@gmail.com>
2023-09-07 23:24:55 +0800

    frame5

--- global.gd
+++ global.gd
@@ -6,8 +6,9 @@ var damp = 5 #每幀pix
 
 
 var speed:float = 3 #每幀pix
+var run_speed:float = 18
 var max_speed:float = 100 
-var max_run_speed:float = 160
+var max_run_speed:float = 300
 var player_position:Vector2 = Vector2.ZERO
 var move_vector:Vector2 = Vector2.ZERO
 var running:bool = false
